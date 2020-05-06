9594bd64208c85857c3ec804b182c60f0bdfa068
Tanner Griffin <tannermgriffin@gmail.com>
2020-05-06 00:54:32 -0500

    created nextQuestion function, questionArray, and setAttribute for answer buttons to run new function onclick

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,27 +11,42 @@ answer1 = document.createElement('button');
 answer2 = document.createElement('button');
 answer3 = document.createElement('button');
 answer4 = document.createElement('button');
-var secondsLeft = 4;
+var secondsLeft = 40;
+var choice = 0;
+var questionIndex = 0;
 
 answerArray = [answer1, answer2, answer3, answer4];
-choiceText = ['example asnwer', 'example answer2', 'example answer3', 'example answer4'];
+choiceText = ['example answer', 'example answer2', 'example answer3', 'example answer4', 'example answer5', 'example answer6', 'example answer7', 'example answer8', 'example answer9', 'example answer10', 'example answer11', 'example answer12', 'example answer13', 'example answer14', 'example answer15', 'example answer16',];
+questionArray = ['What is a method?', 'What is a function?', 'What is the difference between let and var?', 'What is an array?'];
 
 answerList.setAttribute('class', 'list-group flex-center');
 
 startBtn.onclick = function startQuiz() {
 	startTimer();
-	header.textContent = 'What is a method?';
+	header.textContent = questionArray[questionIndex];
 	intro.remove();
 	startBtn.remove();
 	quiz.appendChild(answerList);
 
-	for (i = 0; i < answerArray.length; i++) {
-		answerArray[i].setAttribute('class', 'list-group-item list=group-item-action');
-		answerArray[i].setAttribute('type', 'button');
-		answerList.appendChild(answerArray[i]);
-		answerArray[i].textContent = choiceText[i];
-	}
+	for (choice = 0; choice < answerArray.length; choice++) {
+		answerArray[choice].setAttribute('class', 'list-group-item list=group-item-action');
+        answerArray[choice].setAttribute('type', 'button');
+        answerArray[choice].setAttribute('onclick', 'nextQuestion()');
+        answerList.appendChild(answerArray[choice]);
+		answerArray[choice].textContent = choiceText[choice];
+    }
 };
+
+function nextQuestion() {
+    header.textContent = questionArray[++questionIndex];
+    for (n = 0; n < answerArray.length; n++) {
+        answerArray[n].textContent = choiceText[choice];
+        choice++;
+        if (choice > 16){
+            endQuiz();
+        }
+	}
+}
 
 function startTimer() {
 	var timerInterval = setInterval(function () {
