b221a6484c5a010e313118b6604b11f0616fbaaa
Tanner Griffin <tannermgriffin@gmail.com>
2020-05-13 20:16:29 -0500

    added comments, removed some console.logs, and added a time penalty to wrong answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-var header, intro, startBtn, quiz, answerList, answer1, answer2, answer3, answer4, timer, score, leaderboard;
+var header,
+	intro,
+	startBtn,
+	submitBtn,
+	quiz,
+	form,
+	result,
+	hiscoreEl,
+	hiscoreNum,
+	hiscoreName,
+	leaderList,
+	answerList,
+	answer1,
+	answer2,
+	answer3,
+	answer4,
+	timer,
+	score,
+	leaderboard,
+	timerInterval,
+	timeout,
+	questionNum,
+	randomQuestion;
 
+// grab and create elements
 timer = document.getElementById('timer');
 header = document.getElementById('header');
 intro = document.getElementById('intro');
@@ -21,11 +44,10 @@ answer3 = document.createElement('button');
 answer4 = document.createElement('button');
 
 var secondsLeft = 10;
+var timePenalty = 0;
 var choice = 0;
 var questionIndex = 0;
-var timerInterval;
-var timeout;
-
+// Quiz array full of questions, answers, and choices
 var quizData = [
 	{
 		question: 'Inside which HTML element do we put the JavaScript?',
@@ -86,26 +108,26 @@ var quizData = [
 		question: 'What is the value of the following expression: 8 % 3',
 		answer: '2',
 		choices: ['5', '2', '24', 'null'],
-	}
+	},
 ];
-var questionNum;
-var randomQuestion;
 var answerArray = [answer1, answer2, answer3, answer4];
 var leaderArray = [];
 var totalChoices = 0;
+// fill array with all available choices for endquiz conditional
 for (p = 0; p < quizData.length; p++) {
 	totalChoices += quizData[p].choices.length;
 }
-
+// call high score from local storage, if none exists do not display high score element
 if (localStorage.getItem('hiScore') === null) {
 	hiscoreEl.style.display = 'none';
 } else {
 	hiscoreNum.innerText = JSON.parse(localStorage.getItem('hiScore')).hiscore;
 	hiscoreName.innerText = JSON.parse(localStorage.getItem('hiScore')).initials;
 }
+// add attributes to new answer list div
 answerList.setAttribute('class', 'list-group flex-center');
-
-function randomize () {
+// randomize order of questions
+function randomize() {
 	// clear arrays
 	questionNum = [];
 	randomQuestion = [];
@@ -119,10 +141,8 @@ function randomize () {
 		randomQuestion.push(questionNum[randomNum]);
 		questionNum.splice(randomNum, 1);
 	}
-	console.log(questionNum);
-	console.log(randomQuestion);
 }
-
+// start quiz on start button click
 startBtn.onclick = function startQuiz() {
 	randomize();
 	score = 0;
@@ -133,7 +153,7 @@ startBtn.onclick = function startQuiz() {
 	quiz.appendChild(answerList);
 	answerList.style = '';
 	hiscoreEl.style.display = 'none';
-
+	// append answer list with answer buttons until meeting the answerArray length
 	for (choice = 0; choice < answerArray.length; choice++) {
 		answerArray[choice].setAttribute('class', 'list-group-item list=group-item-action');
 		answerArray[choice].setAttribute('type', 'button');
@@ -142,34 +162,36 @@ startBtn.onclick = function startQuiz() {
 		answerArray[choice].textContent = quizData[randomQuestion[0]].choices[choice];
 	}
 };
-
+// scoring and result text conditionals
+// 10 pts for a correct answer + 1pt for every second left on the question
+// if wrong, reduce the next question timer by 1 second. This effect stacks until a correct answer is made.
 function answerClick(event) {
 	if (event.target.innerText === quizData[randomQuestion[questionIndex]].answer) {
 		result.innerText = 'Correct!';
-		score += (10 + secondsLeft);
-		console.log(score);
+		score += 10 + secondsLeft;
+		timePenalty = 0;
 		timeout = setTimeout(() => {
 			result.innerText = '';
 		}, 1000);
 	} else {
-		result.innerText = 'Wrong!';
-
-		console.log(score);
+		result.innerText = 'Wrong! Time Penalty!';
+		timePenalty += 1;
 		timeout = setTimeout(() => {
 			result.innerText = '';
 		}, 1000);
 	}
 	nextQuestion();
 }
-
+// move to next question and answers in quizData using the random question numbers as indices
 function nextQuestion() {
-	secondsLeft = 10;
+	secondsLeft = 10 - timePenalty;
 	startTimer();
 	if (questionIndex < quizData.length - 1) {
 		header.textContent = quizData[randomQuestion[++questionIndex]].question;
 	}
 	for (n = 0; n < answerArray.length; n++) {
 		choice++;
+		// if reaches the final choice, end the quiz
 		if (choice > totalChoices) {
 			endQuiz();
 			return;
@@ -177,11 +199,15 @@ function nextQuestion() {
 		answerArray[n].textContent = quizData[randomQuestion[questionIndex]].choices[n];
 	}
 }
-
+// start the question timer
+// I initially had it as a full quiz timer, 
+// but I wanted an extra challenge and I liked this scoring system better,
+// so I went with a timer each question.
 function startTimer() {
 	timer.textContent = 'Timer: ' + secondsLeft;
 	clearInterval(timerInterval);
 	timerInterval = setInterval(function () {
+		// if the time runs out on question, move to next question
 		if (secondsLeft === 1 && choice < totalChoices) {
 			result.innerText = "Time's Up!";
 			timeout = setTimeout(() => {
@@ -189,45 +215,55 @@ function startTimer() {
 			}, 1000);
 			nextQuestion();
 		}
+		// if the timer runs out on the last question, end quiz
 		if (secondsLeft === 1 && choice === totalChoices) {
 			clearInterval(timerInterval);
 			endQuiz();
 		}
+		// decrement seconds left if not the last question and time is left
 		if (secondsLeft > 0 && choice <= totalChoices) {
 			secondsLeft = secondsLeft - 1;
 			timer.textContent = 'Timer: ' + secondsLeft;
 		}
 	}, 1000);
 }
-
+// clear question and answers
 function endQuiz() {
 	timer.textContent = 'Timer: 0';
 	header.textContent = 'Score = ' + score;
 	answerList.style.display = 'none';
 	scoreForm();
 }
-
+// display score and input for leaderboard
 function scoreForm() {
 	form.style.display = 'flex';
+	// if local storage has a high score, display it here
 	if (localStorage.getItem('hiScore') === null) {
 	} else {
 		hiscoreEl.style = '';
 	}
 }
-
+// submit user's input to array and local storage
 function addLeader(event) {
 	event.preventDefault();
 	leaderArray.push({ initials: initials.value, score: score });
-	var hiScore = { 
-		initials: initials.value, 
-		hiscore: Math.max.apply(Math, leaderArray.map(function(o) { return o.score; }))
+	var hiScore = {
+		initials: initials.value,
+		hiscore: Math.max.apply(
+			Math,
+			leaderArray.map(function (o) {
+				return o.score;
+			})
+		),
 	};
+	// if no high score exist in local storage, add new score and display
 	if (localStorage.getItem('hiScore') === null) {
 		localStorage.setItem('hiScore', JSON.stringify(hiScore));
 		hiscoreNum.innerText = hiScore.hiscore;
 		hiscoreName.innerText = hiScore.initials;
 	}
-	if (score > JSON.parse(localStorage.getItem('hiScore')).hiscore){
+	// if new score is greater than one in local storage, replace it display
+	if (score > JSON.parse(localStorage.getItem('hiScore')).hiscore) {
 		localStorage.removeItem('hiScore');
 		localStorage.setItem('hiScore', JSON.stringify(hiScore));
 		hiscoreNum.innerText = hiScore.hiscore;
@@ -235,10 +271,11 @@ function addLeader(event) {
 	}
 	leaderboard.style.display = 'grid';
 	form.style.display = 'none';
+	// add list item to list element and write initals and score
 	leaderboard.children[0].appendChild(document.createElement('li'));
 	leaderboard.children[0].lastChild.innerText = initials.value + ' - ' + score;
 }
-
+// return to beginning of quiz
 function goBack() {
 	// reset timer and question index
 	secondsLeft = 10;
@@ -252,14 +289,14 @@ function goBack() {
 	leaderboard.style.display = 'none';
 	hiscoreEl.style = '';
 }
-
+// clear the leaderboard (not the localStorage high score)
 function clearBoard() {
 	var leaderLength = leaderArray.length;
 	for (i = 0; i < leaderLength; i++) {
 		leaderboard.children[0].lastChild.remove();
 	}
 }
-
+// display leaderboard for link in top left of window
 function viewLeaderboard() {
 	timer.textContent = 'Timer: 0';
 	header.textContent = 'Leader Board';
